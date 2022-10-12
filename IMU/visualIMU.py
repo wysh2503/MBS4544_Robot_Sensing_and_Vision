270319600355a2b57a0584a184a1ddb95073a3a3
Winston Yeung <74127616+wysh2503@users.noreply.github.com>
2022-10-12 16:38:25 +0800

    Update visualIMU.py

--- IMU/visualIMU.py
+++ IMU/visualIMU.py
@@ -3,57 +3,59 @@ from time import *
 import numpy as np
 import math
 import serial
-arduinoData = serial.Serial('com6',115200)
+arduinoData = serial.Serial('com4',115200)
 sleep(1)
 
 scene.range=5
-scene.background=color.yellow
+scene.background=color.gray(.2)
 scene.forward=vector(-1,-1,-1)
 
 scene.width=1200
 scene.height=1080
 
-xarrow = arrow(length=2, shaftwidth=.1, color=color.red,axis=vector(1,0,0))
-yarrow = arrow(length=2, shaftwidth=.1, color=color.green,axis=vector(0,1,0))
-zarrow = arrow(length=2, shaftwidth=.1, color=color.blue,axis=vector(0,0,1))
+xarrow = arrow(length=2, shaftwidth=.1, color=color.red, axis=vector(1,0,0))
+yarrow = arrow(length=2, shaftwidth=.1, color=color.green, axis=vector(0,1,0))
+zarrow = arrow(length=2, shaftwidth=.1, color=color.blue, axis=vector(0,0,1))
 
-frontArrow=arrow(length=4,shaftwidth=.1,color=color.purple,axis=vector(1,0,0))
-upArrow=arrow(length=1,shaftwidth=.1,color=color.magenta,axis=vector(0,1,0))
-sideArrow=arrow(length=1,shaftwidth=.1,color=color.orange,axis=vector(0,0,1))
+label(pos=vec(0,-2.5,0), color=color.blue, text='<b>IVE(LWL) - Department of Engineering<b>', height=50)
 
-breadBoard = box(length=6,width=2,height=.2,opacity=.8,pos=vector(0,0,0,))
-IMUsensor = box(length=1,width=.75,height=.1, pos=vector(-.5,.1+.05,0),color=color.blue)
-Arduino = box(length=1.5,width=1.2,height=.1,pos=vector(-2.2,.1+.05,0),color=color.green)
+frontArrow = arrow(length=2,shaftwidth=.1, color=color.purple, axis=vector(1,0,0))
+upArrow = arrow(length=2,shaftwidth=.1, color=color.magenta, axis=vector(0,1,0))
+sideArrow = arrow(length=2,shaftwidth=.1, color=color.orange, axis=vector(0,0,1))
+
+breadBoard = box(length=2.5, width=5, height=.2, opacity=.8, pos=vector(0,0,0,))
+IMUsensor = box(length=.7, width=1, height=.1, pos=vector(-.2,0.1+.05,0), color=color.blue)
+Arduino = box(length=1.2, width=1.5, height=.1, pos=vector(0,0.1+0.05,1.5), color=color.green)
 
 myObj = compound([breadBoard, IMUsensor, Arduino])
 
 while (True):
     try:
-        while (arduinoData.inWaiting()==0):
+        while arduinoData.inWaiting() == 0:
             pass
-        dataPacket=arduinoData.readline()
-        dataPacket=str(dataPacket,'utf-8')
-        splitPacket=dataPacket.split(",")
-        pitch = float(splitPacket[15])*2*np.pi/360.
-        roll = float(splitPacket[16])*2*np.pi/360.
-        yaw = float(splitPacket[17])*2*np.pi/360.
+        dataPacket = arduinoData.readline()
+        dataPacket = str(dataPacket, 'utf-8')
+        splitPacket = dataPacket.split(",")
+        pitch = float(splitPacket[7])*2*np.pi/360.
+        roll = float(splitPacket[8])*2*np.pi/360.
+        yaw = -float(splitPacket[9])*2*np.pi/360.
         print("Roll=", roll / (2*np.pi/360.), " Pitch=", pitch / (2*np.pi/360.),
-              "Yaw=",yaw / (2*np.pi/360.))
+              "Yaw=", yaw / (2*np.pi/360.))
 
         rate(50)
-        k=vector(cos(yaw)*cos(pitch), sin(pitch),sin(yaw)*cos(pitch))
-        y=vector(0,1,0)
-        s=cross(k,y)
-        v=cross(s,k)
-        vrot=v*cos(roll)+cross(k,v)*sin(roll)
+        k = vector(cos(yaw)*cos(pitch), sin(pitch),sin(yaw)*cos(pitch))
+        y = vector(0, 1, 0)
+        s = cross(k, y)
+        v = cross(s, k)
+        vrot = v*cos(roll)+cross(k, v)*sin(roll)
 
-        frontArrow.axis=k
-        sideArrow.axis=cross(k,vrot)
-        upArrow.axis=vrot
-        myObj.axis=k
-        myObj.up=vrot
-        sideArrow.length=1
-        frontArrow.length=4
-        upArrow.length=1
+        frontArrow.axis = k
+        sideArrow.axis = cross(k, vrot)
+        upArrow.axis = vrot
+        myObj.axis = k
+        myObj.up = vrot
+        sideArrow.length = 3
+        frontArrow.length = 4
+        upArrow.length = 3
     except:
         pass
