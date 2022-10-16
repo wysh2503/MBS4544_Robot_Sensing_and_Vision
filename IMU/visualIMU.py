425c7cc1542cd799b8bdc5df633205c684726257
Winston Yeung <74127616+wysh2503@users.noreply.github.com>
2022-10-16 10:50:23 +0800

    Update visualIMU.py

--- IMU/visualIMU.py
+++ IMU/visualIMU.py
@@ -3,7 +3,7 @@ from time import *
 import numpy as np
 import math
 import serial
-arduinoData = serial.Serial('com4',115200)
+arduinoData = serial.Serial('com4',115200) #change com port
 sleep(1)
 
 scene.range=5
@@ -13,17 +13,17 @@ scene.forward=vector(-1,-1,-1)
 scene.width=1200
 scene.height=1080
 
-xarrow = arrow(length=2, shaftwidth=.1, color=color.red, axis=vector(1,0,0))
-yarrow = arrow(length=2, shaftwidth=.1, color=color.green, axis=vector(0,1,0))
-zarrow = arrow(length=2, shaftwidth=.1, color=color.blue, axis=vector(0,0,1))
+xarrow = arrow(length=2, shaftwidth=.15, color=color.red, axis=vector(1,0,0))
+yarrow = arrow(length=2, shaftwidth=.15, color=color.green, axis=vector(0,1,0))
+zarrow = arrow(length=2, shaftwidth=.15, color=color.blue, axis=vector(0,0,1))
 
-label(pos=vec(0,-2.5,0), color=color.blue, text='<b>IVE(LWL) - Department of Engineering<b>', height=50)
+label(pos=vec(0,-2.5,0), color=color.blue, text='<b>IVE(Lee Wai Lee)\nDepartment of Engineering<b>', height=50)
 
 frontArrow = arrow(length=2,shaftwidth=.1, color=color.purple, axis=vector(1,0,0))
 upArrow = arrow(length=2,shaftwidth=.1, color=color.magenta, axis=vector(0,1,0))
 sideArrow = arrow(length=2,shaftwidth=.1, color=color.orange, axis=vector(0,0,1))
 
-breadBoard = box(length=2.5, width=5, height=.2, opacity=.8, pos=vector(0,0,0,))
+breadBoard = box(length=2.5, width=5, height=.2, opacity=.8, pos=vector(0,0,0), color=color.yellow)
 IMUsensor = box(length=.7, width=1, height=.1, pos=vector(-.2,0.1+.05,0), color=color.blue)
 Arduino = box(length=1.2, width=1.5, height=.1, pos=vector(0,0.1+0.05,1.5), color=color.green)
 
@@ -36,7 +36,7 @@ while (True):
         dataPacket = arduinoData.readline()
         dataPacket = str(dataPacket, 'utf-8')
         splitPacket = dataPacket.split(",")
-        pitch = float(splitPacket[7])*2*np.pi/360.
+        pitch = float(splitPacket[7])*2*np.pi/360. # change recevived data starting value [i]
         roll = float(splitPacket[8])*2*np.pi/360.
         yaw = -float(splitPacket[9])*2*np.pi/360.
         print("Roll=", roll / (2*np.pi/360.), " Pitch=", pitch / (2*np.pi/360.),
